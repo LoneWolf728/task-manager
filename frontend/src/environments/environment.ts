8d14b8f32cf814d880209eeb2b99954779e399cd
LoneWolf728 <dineths728@gmail.com>
2025-07-09 21:56:29 +0530

    fix: Remove trailing slash from API URL in development environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@
  */
 export const environment = {
   production: false,  // Flag indicating this is not the production environment
-  apiUrl: 'https://task-manager-heroku-7528c7041b91.herokuapp.com/',  // Base URL for API requests in development environment
+  apiUrl: 'https://task-manager-heroku-7528c7041b91.herokuapp.com',  // Base URL for API requests in development environment
 };
