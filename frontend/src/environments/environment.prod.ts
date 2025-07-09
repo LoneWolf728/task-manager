6f0735494b65f520643216a4f3b667bef0b25804
LoneWolf728 <dineths728@gmail.com>
2025-07-09 21:22:45 +0530

    fix(frontend): Directly set API URL in production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@
  */
 export const environment = {
   production: true,
-  apiUrl: process.env['NG_APP_API_URL'] || 'https://task-manager-heroku-7528c7041b91.herokuapp.com'
+  apiUrl: 'https://task-manager-heroku-7528c7041b91.herokuapp.com'
 };
